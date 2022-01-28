73f82278849e11e5af925c7a10fdc338dbd2d0ac
reuben <creubend@gmail.com>
2022-01-28 12:10:58 -0700

    Complete oop.rb and self eval

--- oop.rb
+++ oop.rb
@@ -2,7 +2,25 @@
 # it should have a dynamic name attribute
 #  it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn
+  attr_accessor :name
+  attr_reader :color
 
+  def initialize(name)
+    @name = name
+    @color = "silver"
+  end
+
+  def say(say)
+    "*~* #{say}"
+  end
+end
+
+unicorn = Unicorn.new("Pointy")
+
+puts unicorn.say("hello")
+
+p unicorn
 
 
 #  Write a class called Vampire
@@ -10,6 +28,27 @@
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+class Vampire
+  attr_accessor :name
+
+  def initialize(name, pet = "bat")
+    @name = name
+    @pet =  pet
+    @thirsty = true
+  end
+
+  def drink()
+    @thirsty = false
+  end
+end
+
+vampire = Vampire.new("Frank", "swan")
+
+
+vampire.drink
+p vampire
+
+
 
 
 #  Write a Dragon class
@@ -18,7 +57,31 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+class Dragon
+  attr_accessor :"name", :"rider", :"color"
 
+  def initialize(name, rider, color)
+    @name = name
+    @rider = rider
+    @color = color
+    @hungry = true
+    @eat_count = 0
+  end
+  def eat()
+    @eat_count = @eat_count + 1
+    if @eat_count == 4
+      @hungry = false
+    end
+  end
+end
+  dragon = Dragon.new("Hiccup", "Rick", "black")
+
+  p dragon.eat
+  p dragon.eat
+  p dragon.eat
+  p dragon.eat
+  p dragon.eat
+  p dragon
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +91,41 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit
+ attr_accessor :"name", :"disposition"
+
+  def initialize(name, disposition, age = 0)
+    @name = name
+    @disposition = disposition
+    @age = age
+    @is_adult = false
+    @is_old = false
+    @has_ring = false
+  end
+
+
+  def birthday()
+    @age = @age + 1
+    if @age > 101
+      @is_old = true
+    elsif @age >= 33
+       @is_adult = true
+    end
+
+
+  if @name == "Frodo"
+       @has_ring = true
+  end
+end
+
+
+   end
+
+   hobbit = Hobbit.new("Frodo", "adventerous", 139)
+
+
+hobbit.birthday
+hobbit.birthday
+hobbit.birthday
+hobbit.birthday
+p hobbit
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,9 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+I felt like I had some very productive struggles, making sure I tried utilizing as many different resources as possible.
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+I didn't take any breaks working through this assignment. It really did just seemed to flow much better than yesterday, and at no point did I feel like a break was needed.
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+I feel like as long as I can take notes and have the time to really absorb the information, then I should do fairly well. Having a different quiet workspace has also been very helpful. 
